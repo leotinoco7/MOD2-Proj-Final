89eb70e909657646404ff60f5afd9c37d515e3ef
anabeatrizalmeida <ana-almeida@hotmail.com>
2022-03-29 16:04:22 -0300

    alteração rotas

--- src/controllers/SeriesController.js
+++ src/controllers/SeriesController.js
@@ -1,4 +1,29 @@
+const res = require("express/lib/response");
 const Series = require("../models/Series");
+
+const login = (req, res) => {
+  try {
+    res.render("login");
+  } catch (err) {
+    res.status(500).send({ err: err.message });
+  }
+};
+
+const login_ = async (req, res) => {
+  try {
+    let usuario = req.body.usuario;
+    let senha = req.body.senha;
+
+    if (!login_) {
+      return res.redirect("/login");
+    }
+
+    await Series.login_(usuario), await Series.login_(senha);
+    res.redirect("/");
+  } catch (err) {
+    res.status(500).send({ err: err.message });
+  }
+};
 
 const getAll = async (req, res) => {
   try {
@@ -9,6 +34,42 @@ const getAll = async (req, res) => {
   }
 };
 
+const cadastro = (req, res) => {
+  try {
+    res.render("cadastro");
+  } catch (err) {
+    res.status(500).send({ err: err.message });
+  }
+};
+
+const create = async (req, res) => {
+  try {
+    const serie = req.body;
+
+    if (!serie) {
+      return res.redirect("/cadastro");
+    }
+
+    await Series.create(serie);
+    res.redirect("/");
+  } catch (err) {
+    res.status(500).send({ err: err.message });
+  }
+};
+
+const detalhes = (req, res) => {
+  try {
+    res.render("detalhes");
+  } catch (err) {
+    res.status(500).send({ err: err.message });
+  }
+};
+
 module.exports = {
   getAll,
+  cadastro,
+  create,
+  detalhes,
+  login,
+  login_,
 };
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,12 @@
 const routes = require("express").Router();
 const SeriesController = require("../controllers/SeriesController");
 
-routes.get("/home", SeriesController.getAll);
+routes.get("/login", SeriesController.login);
+routes.post("/login_", SeriesController.login_);
+routes.get("/", SeriesController.getAll);
+routes.get("/cadastro", SeriesController.cadastro);
+routes.post("/create", SeriesController.create);
+routes.get("/detalhes", SeriesController.detalhes);
 
 module.exports = routes;
+
