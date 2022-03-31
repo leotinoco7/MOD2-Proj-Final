2f4366318668362942caac30263fd8c1602d430c
leotinoco7 <leofptinoco7@gmail.com>
2022-03-31 20:55:05 -0300

    updates

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,12 +1,9 @@
 const routes = require("express").Router();
 const SeriesController = require("../controllers/SeriesController");
 
-routes.get("/index", SeriesController.login);
-routes.post("/acesso", SeriesController.acesso);
-routes.get("/home", SeriesController.getAll);
+routes.get("/", SeriesController.getAll);
 routes.get("/cadastro", SeriesController.cadastro);
-routes.post("/cadastro", SeriesController.create);
-routes.get("/detalhes", SeriesController.detalhes);
+routes.post("/create", SeriesController.create);
 routes.get("/getById/:id/:method", SeriesController.getById);
 
 module.exports = routes;
