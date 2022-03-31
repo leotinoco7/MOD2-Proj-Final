7f2af28523f5d2497274075dc7d349df760dacd6
leotinoco7 <leofptinoco7@gmail.com>
2022-03-31 20:37:47 -0300

    a

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,7 +5,7 @@ routes.get("/index", SeriesController.login);
 routes.post("/acesso", SeriesController.acesso);
 routes.get("/home", SeriesController.getAll);
 routes.get("/cadastro", SeriesController.cadastro);
-routes.post("/create", SeriesController.create);
+routes.post("/cadastro", SeriesController.create);
 routes.get("/detalhes", SeriesController.detalhes);
 routes.get("/getById/:id/:method", SeriesController.getById);
 
