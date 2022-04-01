fc385d635772041f985da09a253f640d5a14d44b
leotinoco7 <leofptinoco7@gmail.com>
2022-03-31 21:35:09 -0300

    routes

--- src/controllers/SeriesController.js
+++ src/controllers/SeriesController.js
@@ -4,7 +4,7 @@ const Series = require('../models/Series');
 const getAll = async (req, res) => {
   try {
     const series_ = await Series.findAll();
-    res.render("/", { series_ });
+    res.render("index", { series_ });
   } catch (err) {
     res.status(500).send({ err: err.message });
   }
