e8d1a090e1579e6f92b3d10bad6b6a0390db8056
leotinoco7 <leofptinoco7@gmail.com>
2022-03-31 22:18:00 -0300

    a

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const app = express();
-const session = require("express-session");
-const bodyParser = require("body-parser");
 const routes = require("./src/routes/routes");
 const port = process.env.PORT || 3000;
 
@@ -12,8 +10,6 @@ const port = process.env.PORT || 3000;
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded());
-app.use(session({secret: 'user157'}));
-app.use(bodyParser.urlencoded({extended:true}));
 app.use(routes);
 
 
