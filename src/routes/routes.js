a4399f546cdbbf5388493eadb2ad1b5ec5676266
leotinoco7 <leofptinoco7@gmail.com>
2022-03-31 22:40:17 -0300

    a

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,7 @@
 const routes = require("express").Router();
 const SeriesController = require("../controllers/SeriesController");
 
-routes.get("/index", SeriesController.getAll);
+routes.get("/", SeriesController.getAll);
 routes.get("/cadastro", SeriesController.cadastro);
 routes.post("/create", SeriesController.create);
 // routes.get("/Edit/:id/:method", SeriesController.Edit);
