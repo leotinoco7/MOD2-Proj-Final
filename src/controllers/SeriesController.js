e4f210ecf7ea3d58da8a0e4501211151d975909f
mthiagoalves <m.thiago.alves@live.com>
2022-04-04 20:34:37 -0300

    andamento

--- src/controllers/SeriesController.js
+++ src/controllers/SeriesController.js
@@ -7,17 +7,22 @@ const series_ = Series.findAll();
 const getAll = async (req, res) => {
   try {
     const series_ = await Series.findAll();
+
+    let randomSeries;
+
+    currentRandom = [Math.floor(Math.random() * series_.length)];
+
+    randomSeries = series_[currentRandom];
+
+    console.log(currentRandom);
+
     res.render("index", { series_ });
+
+    
   } catch (err) {
     res.status(500).send({ err: err.message });
   }
-  let randomSeries;
-
-  currentRandom = [Math.floor(Math.random() * series_.length)];
-
-  randomSeries = series_[currentRandom]
-
-  console.log(currentRandom);
+  
 
 };
 
